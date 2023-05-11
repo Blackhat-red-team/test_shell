523c5bd2b7ba4c5b8f024eddf932ad28a8f8b645
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-05-11 07:41:43 +0300

    first

--- simple_shell_with_arguments.c
+++ simple_shell_with_arguments.c
@@ -6,24 +6,29 @@
 
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGUMENTS 10
-
+/*
+* main - Entry point of the program.
+*/
 int main(void)
 {
 char command[MAX_COMMAND_LENGTH];
 char *args[MAX_ARGUMENTS + 1];
 
-while (1) {
+while (1)
+{
 printf("simple_shell$ ");
-if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+{
 printf("\n");
 break;
 }
 
-command[strcspn(command, "\n")] = '\0'; 
+command[strcspn(command, "\n")] = '\0';
 
 char *token = strtok(command, " ");
 int i = 0;
-while (token != NULL && i < MAX_ARGUMENTS) {
+while (token != NULL && i < MAX_ARGUMENTS)
+{
 args[i] = token;
 token = strtok(NULL, " ");
 i++;
@@ -31,16 +36,22 @@ i++;
 args[i] = NULL;
 
 pid_t pid = fork();
-if (pid < 0) {
+if (pid < 0)
+{
 perror("fork");
 exit(EXIT_FAILURE);
-} else if (pid == 0) {
+}
+else if (pid == 0)
+{
 
-if (execvp(args[0], args) == -1) {
+if (execvp(args[0], args) == -1)
+{
 perror("execvp");
 exit(EXIT_FAILURE);
 }
-} else {
+}
+else
+{
 
 wait(NULL);
 }
