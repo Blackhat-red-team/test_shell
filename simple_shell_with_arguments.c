21b6dda53350e950243563e137f2594b213b279a
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-05-11 07:30:50 +0300

    first

--- simple_shell_with_arguments.c
+++ simple_shell_with_arguments.c
@@ -7,43 +7,44 @@
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGUMENTS 10
 
-int main(void) {
-    char command[MAX_COMMAND_LENGTH];
-    char *args[MAX_ARGUMENTS + 1];
+int main(void)
+{
+char command[MAX_COMMAND_LENGTH];
+char *args[MAX_ARGUMENTS + 1];
 
-    while (1) {
-        printf("simple_shell$ ");  // Display the prompt
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
-            printf("\n");  // Handle EOF (Ctrl+D)
-            break;
-        }
+while (1) {
+printf("simple_shell$ ");
+if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+printf("\n");
+break;
+}
 
-        command[strcspn(command, "\n")] = '\0';  // Remove trailing newline
+command[strcspn(command, "\n")] = '\0'; 
 
-        char *token = strtok(command, " ");
-        int i = 0;
-        while (token != NULL && i < MAX_ARGUMENTS) {
-            args[i] = token;
-            token = strtok(NULL, " ");
-            i++;
-        }
-        args[i] = NULL;
+char *token = strtok(command, " ");
+int i = 0;
+while (token != NULL && i < MAX_ARGUMENTS) {
+args[i] = token;
+token = strtok(NULL, " ");
+i++;
+}
+args[i] = NULL;
 
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
-            if (execvp(args[0], args) == -1) {
-                perror("execvp");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            // Parent process
-            wait(NULL);  // Wait for the child to complete
-        }
-    }
+pid_t pid = fork();
+if (pid < 0) {
+perror("fork");
+exit(EXIT_FAILURE);
+} else if (pid == 0) {
 
-    return 0;
+if (execvp(args[0], args) == -1) {
+perror("execvp");
+exit(EXIT_FAILURE);
+}
+} else {
+
+wait(NULL);
+}
+}
+
+return 0;
 }
